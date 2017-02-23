f0a5fe5c0fef0f0f216f242f7f8caf7b53ae5a23
Megaju <megaju@kijewoku.fr>
2017-02-23 16:36:39 +0100

    jeu de test + affichage users et planets

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Warcredits</title>
+  <link rel="stylesheet" href="css/master.css">
+</head>
+<!-- connect to db -->
+<?php include('db.php'); ?>
+<body>
+  <header>
+    <h1>WARCREDITS</h1>
+    <h2>Un jeu où tu te bat pour du fric de banquier pour te battre plus encore !</h2>
+  </header>
+
+  <section>
+    <div class="inscription">
+      <h3>Inscription</h3>
+      <form action="index.html" method="post">
+        <input type="text" name="name" placeholder="Name">
+        <input type="text" name="planet" placeholder="Planet name">
+        <input type="password" name="pwd" placeholder="Password">
+        <input type="submit" value="Create account">
+      </form>
+    </div>
+    <div class="intro">
+    <!-- users list -->
+    <h3>Utilisateurs enregistrés</h3>
+    <ul>
+      <?php
+      $users = $db->query('SELECT users.id AS userid, users.name AS name, planets.users_id, planets.name AS planet FROM users INNER JOIN planets ON planets.users_id = users.id');
+      while ($user = $users->fetch()) {
+        echo '<li><i class="name">'.$user['name'].'</i> empereur de la planète '.$user['planet'].' !!!</li>';
+      }
+      ?>
+    </ul>
+    </div>
+  </section>
+
+  <footer>
+    <p>Warcredits est un jeu de stratégie fun est "convivial" qui vous est proposé gratuitement par Watilin et Megaju.</p>
+  </footer>
+</body>
+</html>
